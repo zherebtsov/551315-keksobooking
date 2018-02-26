0f74b1fcf62fd2d4e1097f4853e4a3ece4a5bc6d
Dmitry <dmitryje@ya.ru>
2018-02-26 17:57:20 +0700

    module5-task2
    added moving for pin and fixed bugs

--- js/map.js
+++ js/map.js
@@ -4,7 +4,9 @@
   var MAP = window.common.MAP;
   var ESC_KEYCODE = 27;
   var MAP_PIN_MAIN = MAP.querySelector('.map__pin--main');
-  var MAP_PIN_MAIN_ARROW_HEIGHT = 22;
+  var MAP_PIN_MAIN_ARROW_HEIGHT = 18;
+  var MIN_Y = 150;
+  var MAX_Y = 500;
 
   var openPopupCard = function (card) {
     window.card.del();
@@ -46,14 +48,69 @@
     return coords;
   };
 
-  var onMainPinMouseup = function () {
-    window.pageState.enable();
-    window.form.setAddress(getCoordsMainPin());
-    window.pin.render(window.data, openPopupCard);
-    MAP_PIN_MAIN.removeEventListener('mouseup', onMainPinMouseup);
+  var init = function () {
+    if (!window.pageState.check()) {
+      window.pageState.enable();
+      window.pin.render(window.data, openPopupCard);
+    }
   };
 
-  MAP_PIN_MAIN.addEventListener('mouseup', onMainPinMouseup);
+  MAP_PIN_MAIN.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    init();
+
+    var prevCoordsMouse = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var mapSize = MAP.getBoundingClientRect();
+    var pinSize = MAP_PIN_MAIN.getBoundingClientRect();
+    var topEdge = MIN_Y - pinSize.height + MAP_PIN_MAIN_ARROW_HEIGHT;
+    var bottomEdge = MAX_Y - pinSize.height + MAP_PIN_MAIN_ARROW_HEIGHT;
+    var leftEdge = mapSize.left;
+    var rigthEdge = mapSize.left + mapSize.width;
+
+    var onMainPinMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: prevCoordsMouse.x - moveEvt.clientX,
+        y: prevCoordsMouse.y - moveEvt.clientY
+      };
+
+      prevCoordsMouse = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var currentCoordsMouse = {
+        x: prevCoordsMouse.x - shift.x + pageXOffset,
+        y: prevCoordsMouse.y - shift.y + pageYOffset
+      };
+
+      if (currentCoordsMouse.y > topEdge && currentCoordsMouse.y < bottomEdge) {
+        MAP_PIN_MAIN.style.top = currentCoordsMouse.y + 'px';
+      }
+
+      if (currentCoordsMouse.x > leftEdge && currentCoordsMouse.x < rigthEdge) {
+        MAP_PIN_MAIN.style.left = (currentCoordsMouse.x - mapSize.left) + 'px';
+      }
+
+      window.form.setAddress(getCoordsMainPin());
+    };
+
+    var onMainPinMouseup = function (upEvt) {
+      upEvt.preventDefault();
+
+      window.form.setAddress(getCoordsMainPin());
+      MAP.removeEventListener('mousemove', onMainPinMouseMove);
+      document.removeEventListener('mouseup', onMainPinMouseup);
+    };
+
+    MAP.addEventListener('mousemove', onMainPinMouseMove);
+    document.addEventListener('mouseup', onMainPinMouseup);
+  });
 
   window.map = {
     getCoordsMainPin: getCoordsMainPin,
